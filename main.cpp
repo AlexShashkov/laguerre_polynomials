3281a3f877b54520db58f920840f961ee6ea4de3
AlexShashkov <sh@dmitrov-dubna.ru>
2024-04-14 13:17:55 +0300

    Various fixes

--- main.cpp
+++ main.cpp
@@ -70,20 +70,28 @@
     #define TRIES 80 // For how many iterations Laguerre should try to solve polynomial
 #endif
 
+using std::cout;
+using std::vector;
+using std::complex;
+using std::for_each;
+using std::fill;
+using std::exception;
+using std::invalid_argument;
+using std::endl;
+using std::cerr;
 
 int main(){
     using Laguerre::Polynomial;
-    std::streamsize fp_precision_original=std::cout.precision(); // save default precision to provide maximal reasonable output
     
     // Laguerre solvers
     #if SOLVER == 0
-        std::cout << "Using original version of Laguerre\n";
+        cout << "Using original version of Laguerre\n";
         Laguerre::Original<NUMBER>* solver = new Laguerre::Original<NUMBER>();
     #elif SOLVER == 1
-        std::cout << "Using modified version of Laguerre, 2013\n";
+        cout << "Using modified version of Laguerre, 2013\n";
         Laguerre::ModifiedLaguerre13<NUMBER>* solver = new Laguerre::ModifiedLaguerre13<NUMBER>();
     #elif SOLVER == 2
-        std::cout << "Using modified version of Laguerre, 2018\n";
+        cout << "Using modified version of Laguerre, 2018\n";
         Laguerre::ModifiedLaguerre18<NUMBER>* solver = new Laguerre::ModifiedLaguerre18<NUMBER>();
     #endif
 
@@ -94,7 +102,7 @@ int main(){
         N_roots_gt_all_tests_ae_worst=0, N_roots_found_all_tests_ae_worst=0, // {ground truth, found} NUMBER of roots of the worst polynomial for absolute error
         N_roots_gt_all_tests_re_worst=0, N_roots_found_all_tests_re_worst=0; // {ground truth, found} NUMBER of roots of the worst polynomial for relative error
 
-    std::vector<NUMBER> roots(DEGREE, 0.0), roots_found_this_test(DEGREE), a(DEGREE+1, 0.0),                                       // current polynomial
+    vector<NUMBER> roots(DEGREE, 0.0), roots_found_this_test(DEGREE), a(DEGREE+1, 0.0),                                       // current polynomial
         roots_gt_all_tests_ae_worst(DEGREE), roots_found_all_tests_ae_worst(DEGREE), coefficients_all_tests_ae_worst(DEGREE+1),    // {gt roots, found roots, coefficients} of the worst polynomial for absolute error
         roots_gt_all_tests_re_worst(DEGREE), roots_found_all_tests_re_worst(DEGREE), coefficients_all_tests_re_worst(DEGREE+1);    // {gt roots, found roots, coefficients} of the worst polynomial for relative error
 
@@ -109,39 +117,36 @@ int main(){
             N_roots_gt_this_test = generate_polynomial<NUMBER, EXPONENT, MANTISSA>(DEGREE, N_PAIRS_OF_COMPLEX_ROOTS, N_CLUSTERED_ROOTS,
                 N_MULTIPLE_ROOTS, MAX_DISTANCE_BETWEEN_CLUSTERED, ROOT_SWEEP_LOW, ROOT_SWEEP_HIGH, roots, a);
 
-            std::cout << "GENERATED POLY & ROOTS:\n";
+            cout << "GENERATED POLY & ROOTS:\n";
             Laguerre::printVec(a);
             Laguerre::printVec(roots);
             pol.setCoeffs(a);
 
-            std::vector<std::complex<NUMBER>> solved_roots(DEGREE);
-            std::vector<int> conv(DEGREE);
+            vector<complex<NUMBER>> solved_roots(DEGREE);
+            vector<int> conv(DEGREE);
 
             pol.solve(solved_roots, conv, TRIES);
 
-            std::cout << "found roots: ";
-            std::for_each(solved_roots.begin(), solved_roots.end(), [&roots_found_this_test](std::complex<NUMBER> x){ // solved_roots = b_roots
-                std::cout << x.real() << "; ";
+            cout << "found roots: ";
+            for_each(solved_roots.begin(), solved_roots.end(), [&roots_found_this_test](complex<NUMBER> x){ // solved_roots = b_roots
+                cout << x.real() << "; ";
                 roots_found_this_test.push_back(x.real());
             });
 
             N_roots_found_this_test = roots_found_this_test.size();
 
-            std::sort(roots_found_this_test.begin(), roots_found_this_test.end());
-            std::sort(roots.begin(), roots.end());
-
             rv=compare_roots<NUMBER>(N_roots_found_this_test, N_roots_gt_this_test, roots_found_this_test, roots,
                                                                                     ae_this_test_worst, re_this_test_worst);
 
 
-            std::cout << std::endl << "P=" << DEGREE << ", test No " << i << " out of " << N_TESTS << std::endl
+            cout << endl << "P=" << DEGREE << ", test No " << i << " out of " << N_TESTS << endl
                 << "comparison return value=" << (rv==PR_NUMBERS_OF_ROOTS_EQUAL ? "PR_NUMBERS_OF_ROOTS_EQUAL" :
                 (rv==PR_AT_LEAST_ONE_ROOT_LOST ? "PR_AT_LEAST_ONE_ROOT_LOST" : 
                 (rv==PR_AT_LEAST_ONE_ROOT_IS_FAKE ? "PR_AT_LEAST_ONE_ROOT_IS_FAKE" : "UNKNOWN VALUE")))
                 << ", N_roots_gt_this_test=" << N_roots_gt_this_test << ", N_roots_found_this_test=" << N_roots_found_this_test
                 << ", N_roots_verified_this_test=" << DEGREE
                 << "ae_this_test_worst=" << ae_this_test_worst
-                << ", re_this_test_worst=" << re_this_test_worst << " ------------------------------" << std::endl;
+                << ", re_this_test_worst=" << re_this_test_worst << " ------------------------------" << endl;
             
             N_true_roots_lost+=(N_roots_found_this_test<N_roots_gt_this_test)*(N_roots_gt_this_test-N_roots_found_this_test);
             N_fake_roots_added+=(N_roots_found_this_test>N_roots_gt_this_test)*(N_roots_found_this_test-N_roots_gt_this_test);
@@ -157,78 +162,75 @@ int main(){
                 roots_found_all_tests_re_worst=roots_found_this_test; N_roots_found_all_tests_re_worst=N_roots_found_this_test;
                 coefficients_all_tests_re_worst=a; re_all_tests_worst=re_this_test_worst;
             }
-            if(Laguerre::anynotfinite(roots_found_this_test)){
-                std::cout << "\nNOT FINITE ROOTS!!!\n";
+            if(Laguerre::anynotfinite<NUMBER>(roots_found_this_test)){
+                cout << "\nNOT FINITE ROOTS!!!\n";
                 delete solver;
                 return 1;
             }
-            if(Laguerre::anynotfinite(a)){
-                std::cout << "NOT FINITE COEFFS!!!\n";
+            if(Laguerre::anynotfinite<NUMBER>(a)){
+                cout << "NOT FINITE COEFFS!!!\n";
                 delete solver;
                 return 1;
             }
-            std::fill(roots.begin(), roots.end(), 0.0);
-            std::fill(a.begin(), a.end(), 0.0);
+            fill(roots.begin(), roots.end(), 0.0);
+            fill(a.begin(), a.end(), 0.0);
 
-            std::cout << "\n=======================\n";
+            cout << "\n=======================\n";
         }
 
     }
-    catch (const std::invalid_argument &exc)
+    catch (const invalid_argument &exc)
     {
-        std::cerr << "Bad argument: " << exc.what();
+        cerr << "Bad argument: " << exc.what();
     }
-    catch (const std::exception &exc)
+    catch (const exception &exc)
     {
-        std::cerr << exc.what();
+        cerr << exc.what();
     }
-    std::cout << "DONE!\n";
-    std::cout << std::endl << "--------------- settings ----------------" << std::endl
+    cout << "\nDONE!\n";
+    cout << endl << "--------------- settings ----------------" << endl
         << "P=" << DEGREE << ", N_tests=" << N_TESTS; 
         
-    std::cout << std::endl << std::endl << "--------------- statistics ----------------" << std::endl
-        << "N_true_roots_lost=" << N_true_roots_lost << ", N_fake_roots_added=" << N_fake_roots_added
-        << std::endl;
+    cout << endl << endl << "--------------- statistics ----------------" << endl
+        << ", N_true_roots_lost=" << N_true_roots_lost << ", N_fake_roots_added=" << N_fake_roots_added
+        << endl;
 
     // output the worst cases
-    std::cout << std::endl << "--------------- worst case: absolute error: ----------------" << std::endl
+    cout << endl << "--------------- worst case: absolute error: ----------------" << endl
         << "ae_all_tests_worst=" << ae_all_tests_worst
         << ", N_roots_gt_all_tests_ae_worst=" << N_roots_gt_all_tests_ae_worst
-        << ", N_roots_found_all_tests_ae_worst=" << N_roots_found_all_tests_ae_worst << std::endl;
+        << ", N_roots_found_all_tests_ae_worst=" << N_roots_found_all_tests_ae_worst << endl;
 
-    std::cout << std::endl << "--------------- worst case: relative error: ----------------" << std::endl
+    cout << endl << "--------------- worst case: relative error: ----------------" << endl
         << "re_all_tests_worst=" << re_all_tests_worst
         << ", N_roots_gt_all_tests_re_worst=" << N_roots_gt_all_tests_re_worst
-        << ", N_roots_found_all_tests_re_worst=" << N_roots_found_all_tests_re_worst << std::endl;
+        << ", N_roots_found_all_tests_re_worst=" << N_roots_found_all_tests_re_worst << endl;
 
-    std::cout << "\nWorst case method results ae: ";
+    cout << "\nWorst case method results ae: ";
     for (auto &root : roots_found_all_tests_ae_worst){
-        std::cout << root << " ";
+        cout << root << " ";
     }
-    std::cout << "\nGround truth worst ae: {";
+    cout << "\nGround truth worst ae: {";
     for (auto &root : roots_gt_all_tests_ae_worst){
-        std::cout << root << ", ";
+        cout << root << ", ";
     }
-    std::cout << "}\nCoeffs worst ae: {";
+    cout << "}\nCoeffs worst ae: {";
     for (auto &root : coefficients_all_tests_ae_worst){
-        std::cout << root << ", ";
+        cout << root << ", ";
     }
 
-    std::cout << "}\nWorst case method results re: ";
+    cout << "}\nWorst case method results re: ";
     for (auto &root : roots_found_all_tests_re_worst){
-        std::cout << root << " ";
+        cout << root << " ";
     }
-    std::cout << "\nGround truth worst re: {";
+    cout << "\nGround truth worst re: {";
     for (auto &root : roots_gt_all_tests_re_worst){
-        std::cout << root << ", ";
+        cout << root << ", ";
     }
-    std::cout << "}\nCoeffs worst re: {";
+    cout << "}\nCoeffs worst re: {";
     for (auto &root : coefficients_all_tests_re_worst){
-        std::cout << root << ", ";
+        cout << root << ", ";
     }   
-    std::cout << std::setprecision(fp_precision_original); // restore default precision
-
-    
     delete solver;
     return 0;
 }
